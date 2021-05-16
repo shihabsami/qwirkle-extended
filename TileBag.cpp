fd56c50d6ec5193455a368633f20d9ee2a294b02
Shihab Sami <s3823710@student.rmit.edu.au>
2021-05-16 14:14:23 +1000

    Did documentation

--- TileBag.cpp
+++ TileBag.cpp
@@ -16,8 +16,8 @@ TileBag::~TileBag() { tiles.reset(); }
 
 void TileBag::fill() {
     if (tiles->isEmpty()) {
-        for (unsigned int i = 0; i < COLOURS.size(); ++i) {
-            for (unsigned int j = 0; j < SHAPES.size(); ++j) {
+        for (size_t i = 0; i < COLOURS.size(); ++i) {
+            for (size_t j = 0; j < SHAPES.size(); ++j) {
                 // two of each colour and shape combination
                 tiles->addBack(make_shared<Tile>(COLOURS.at(i), SHAPES.at(j)));
                 tiles->addBack(make_shared<Tile>(COLOURS.at(i), SHAPES.at(j)));
@@ -29,8 +29,8 @@ void TileBag::fill() {
 }
 
 void TileBag::shuffle() {
-    for (unsigned int i = 0; i < tiles->size(); ++i) {
-        int randomIndex = getRandomIndex();
+    for (size_t i = 0; i < tiles->size(); ++i) {
+        size_t randomIndex = getRandomIndex();
 
         shared_ptr<Tile> toBeReplaced = tiles->at(i);
         shared_ptr<Tile> randomTile = tiles->at(randomIndex);
@@ -41,23 +41,12 @@ void TileBag::shuffle() {
     }
 }
 
-shared_ptr<Tile> TileBag::getRandomTile() {
-    if (tiles->isEmpty())
-        throw length_error("no remaining tiles for TileBag::getRandomTile");
-
-    int randomIndex = getRandomIndex();
-    shared_ptr<Tile> randomTile = tiles->at(randomIndex);
-    tiles->remove(randomIndex);
-
-    return randomTile;
-}
-
 shared_ptr<PlayerHand> TileBag::getHand() {
     if (tiles->size() < HAND_SIZE)
         throw length_error("insufficient number of tiles for TileBag::getHand");
 
     shared_ptr<PlayerHand> hand = make_shared<PlayerHand>();
-    for (int i = 0; i < HAND_SIZE; ++i) {
+    for (size_t i = 0; i < HAND_SIZE; ++i) {
         hand->addTile(tiles->at(tiles->size() - 1));
         tiles->removeBack();
     }
@@ -68,7 +57,7 @@ shared_ptr<PlayerHand> TileBag::getHand() {
 shared_ptr<LinkedList> TileBag::getTiles() { return tiles; }
 
 shared_ptr<Tile> TileBag::replace(shared_ptr<Tile>& tile) {
-    int randomIndex = getRandomIndex();
+    size_t randomIndex = getRandomIndex();
     shared_ptr<Tile> randomTile = tiles->at(randomIndex);
     tiles->remove(randomIndex);
     tiles->addBack(tile);
@@ -76,9 +65,9 @@ shared_ptr<Tile> TileBag::replace(shared_ptr<Tile>& tile) {
     return randomTile;
 }
 
-unsigned int TileBag::getRandomIndex() {
+size_t TileBag::getRandomIndex() {
     random_device engine;
-    uniform_int_distribution<unsigned int> distribution(0, tiles->size() - 1);
+    uniform_int_distribution<size_t> distribution(0, tiles->size() - 1);
 
     return distribution(engine);
 }
--- TileBag.h
+++ TileBag.h
@@ -9,18 +9,63 @@ class PlayerHand;
 
 class TileBag {
 public:
+    /**
+    * Construct a bag with an empty LinkedList.
+    */
     TileBag();
+
+    /**
+     * Destructor to free the tiles in the bag.
+     */
     ~TileBag();
+
+    /**
+     * Fill in the bag with maximum number of tiles possible.
+     */
     void fill();
+
+    /**
+     * Shuffle the bag using an uniform distribution.
+     */
     void shuffle();
-    shared_ptr<Tile> getRandomTile();
+
+    /**
+     * Get a hand (6) of tiles.
+     *
+     * @return a pointer to PlayerHand
+     */
     shared_ptr<PlayerHand> getHand();
+
+    /**
+     * Get all the tiles in the bag.
+     *
+     * @return a pointer to a LinkedList of tiles
+     * */
     shared_ptr<LinkedList> getTiles();
+
+    /**
+     * Replace a tile from the bag.
+     *
+     * @param tile - the tile that is inserted
+     * @return a pointer to a tile from the bag
+     * */
     shared_ptr<Tile> replace(shared_ptr<Tile>& tile);
+
+    /**
+     * Overloaded output stream operator for easy printing.
+     *
+     * @param os - a reference to the output stream
+     * @param hand - a reference to the TileBag to be printed
+     */
     friend ostream& operator<<(ostream& os, const TileBag& bag);
 
 private:
-    unsigned int getRandomIndex();
+    /**
+     * A random number generator.
+     *
+     * @return an index within the bounds of the bag
+     */
+    size_t getRandomIndex();
 
     shared_ptr<LinkedList> tiles;
 };
